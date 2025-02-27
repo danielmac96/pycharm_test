7cb09ab73d9fc6667469dc9325fa80e0a60f0513
danmaclean <danielmac96@gmail.com>
2025-02-26 23:26:13 -0500

    first commit

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Dan')
+
+print('wow a successful commit')
+
+print("my efmfe")
+
+print("another commit")
+
+def print_my_text(text, repeat=1):
+    for _ in range(repeat):
+        print(text)
+
+print_my_text('Hello, world!', 6)
